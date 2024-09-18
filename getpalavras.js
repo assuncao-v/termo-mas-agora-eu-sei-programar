9bfd6ab5c41875f03783be0315134c25f883bd40
Vítor Assunção <vitor.assuncaosantos100@gmail.com>
2024-09-17 22:37:13 -0300

    2024.09.17.1.assuncao-v--mudancas_nas_funcoes_definePalavraDoDia_e_getPalavra__adicao_da_funcao_limparBuffer_e_da_matriz_palavras

---
+++ getpalavras.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+// const string = fs.readFileSync("palavras.txt", {encoding: "utf-8"}).split("\r\n").filter((value, i) => (value.length == 5 && value[0] != value[0].toUpperCase() && value.match(/[a-z]{5,5}/) && value.match(/(a.*a.*a)|(e.*e.*e)|(i.*i.*i)|(o.*o.*o)|(u.*u.*u)/))).map((value) => ("\"" + value + "\"")).join(',\n');
+
+// const string = 
+
+//const string = fs.readFileSync("palavras.txt", {encoding: "utf-8"}).split("\n").map((value) => value.trim()).map((value, i) => {return `/* ${value} */\ncase ${i}:\nfirstLetter = '${value[0]}';\nsecondLetter = '${value[1]}';\nthirdLetter = '${value[2]}';\nfourthLetter = '${value[3]}';\nfifthLetter = '${value[4]}';\nbreak;`}).join('\n\n');
+
+const string = fs.readFileSync("palavras.txt", {encoding: "utf-8"}).split("\n").map((value) => value.trim()).map((value, i) => {return `{'${value[0]}', '${value[1]}', '${value[2]}', '${value[3]}', '${value[4]}', '\\0' },`}).join('\n');
+
+console.log(string);
+
+fs.writeFileSync("text.txt", string);
